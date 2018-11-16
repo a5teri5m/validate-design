decc138de8d741ee11b099d18a5b002dfc681146
aster_ism <asterism1105@gmail.com>
2018-11-16 21:02:42 +0900

    Bugfix
    - lappendを使用していたためリストが入れ子になり動作しない問題を修正
    - オブジェクトが取得できないときがあったのでget_bd_cellsを使用するように修正

--- validate-design.tcl
+++ validate-design.tcl
@@ -10,7 +10,7 @@ namespace eval asterism {
     proc set_intf_properties {inst corr} {
         dict for {intf_name properties} ${corr} {
             dict for {intf_property mod_property} ${properties} {
-                set value [get_property CONFIG.${mod_property} ${inst}]
+                set value [get_property CONFIG.${mod_property} [get_bd_cells ${inst}]]
                 set_property CONFIG.${intf_property} ${value} [get_bd_intf_pins ${inst}/${intf_name}]
                 puts "INFO: ${intf_name}.${intf_property} <- ${mod_property} = ${value} (${inst})"
             }    
@@ -21,8 +21,8 @@ namespace eval asterism {
         set ips [list]
         foreach cell [get_bd_cells ${hier}*] {
             switch [get_property TYPE ${cell}] {
-            "ip" {lappend ips ${cell}}
-            "hier" {lappend ips [get_bd_ips ${cell}/]}
+            "ip" {set ips [concat ${ips} ${cell}]}
+            "hier" {set ips [concat ${ips} [get_bd_ips ${cell}/]]}
             }   
         }
         return ${ips}
@@ -43,7 +43,7 @@ namespace eval asterism {
         }
         set config [::json::json2dict ${json}]
         foreach inst [get_bd_ips] {
-            set vlnv [get_property VLNV ${inst}]
+            set vlnv [get_property VLNV [get_bd_cells ${inst}]]
             set module [lindex [split ${vlnv} :] 2]
             if [dict exists ${config} ${module}] {
                 set corr [dict get ${config} ${module}]
